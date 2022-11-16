2cdd19af866172bc37e131cd7bda3b9f0112ff20
julien lizano <julienlizano@gmail.com>
2022-11-16 17:33:32 +0100

    modif image + creation liste et recuperation mots js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// button
 var animateButton = function(e) {
 
     e.preventDefault;
@@ -15,3 +16,64 @@ var animateButton = function(e) {
   for (var i = 0; i < bubblyButtons.length; i++) {
     bubblyButtons[i].addEventListener('click', animateButton, false);
   }
+
+  // jeu
+
+  let lettre = document.querySelector('.lettre');
+  let indice = document.querySelector('#indice');
+  let score = document.querySelector('#score');
+  let motATrouver = document.querySelector('#mot');
+  let demarrer = document.querySelector('#partie');
+  let hidden = true;
+  score.textContent = '';
+  
+let dernier   = 0;
+let nombreAleatoire = 0;
+let mots = [
+  ["BASEBALL", "SPORT"],
+  ["TENNIS", "SPORT"],
+  ["PADDLE", "SPORT"],
+  ["JUDO", "SPORT"],
+  ["BASKETBALL", "SPORT"],
+  ["KARATE", "SPORT"],
+  ["FRANCE", "PAYS"],
+  ["ESPAGNE", "PAYS"],
+  ["ALLEMAGNE", "PAYS"],
+  ["CANADA", "PAYS"],
+  ["ANGLETERRE", "PAYS"],
+  ["UKRAINE", "PAYS"],
+  ["POMME", "FRUITS"],
+  ["FRAISE", "FRUITS"],
+  ["BANANE", "FRUITS"],
+  ["ABRICOT", "FRUITS"],
+  ["RAISIN", "FRUITS"],
+  ["ORANGE", "FRUITS"],
+];
+
+function genererNombreEntier(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+demarrer.addEventListener('click', () => {
+  do {
+    nombreAleatoire = genererNombreEntier(mots.length);
+  } while (nombreAleatoire == dernier)
+ 
+  motATrouver.textContent = mots[nombreAleatoire][0];
+  indice.textContent   = mots[nombreAleatoire][1];
+  dernier              = nombreAleatoire;
+
+  if (hidden) {
+    motATrouver.textContent = " _ _ _ _ _ _ _";
+    hidden = false;
+  } else {
+    
+    hidden = true;
+  }
+});
+
+
+
+  lettre.addEventListener('click', () => {
+    console.log(lettre);
+  });
