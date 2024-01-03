3fdabb8715cd4aa9daeeb72e66d687ed07d1ad24
Julien Delsescaux <jdlxt@locahost.dev>
2024-01-03 16:45:51 +0100

    refactorisation

--- script.js
+++ script.js
@@ -1,157 +1,211 @@
 "use strict";
-let alphabet = [].slice.call(document.getElementsByClassName('lettre'));
-let indice = document.querySelector('#indice');
-let demarrer = document.querySelector('#partie');
-let formulaire = document.querySelector('#formulaire');
-let input = document.querySelector('#devine');
-let mot = document.querySelector("#mot");
-let pendu = document.getElementById('pendu');
-
-let stockageMot;
-let leMot;
-let erreur = 0;
-let dernier = 0;
-let nombreAleatoire = 0;
 
 let date = new Date().getFullYear();
 copyright.innerHTML = `${date} © JSL Code`;
 
 
-let mots = [
-  ["BASEBALL", "SPORT"], ["TENNIS", "SPORT"], ["PADDLE", "SPORT"], ["JUDO", "SPORT"], ["BASKETBALL", "SPORT"], ["KARATE", "SPORT"],
-  ["FRANCE", "PAYS"], ["ESPAGNE", "PAYS"], ["ALLEMAGNE", "PAYS"], ["CANADA", "PAYS"], ["ANGLETERRE", "PAYS"], ["UKRAINE", "PAYS"],
-  ["POMME", "FRUITS"], ["FRAISE", "FRUITS"], ["BANANE", "FRUITS"], ["ABRICOT", "FRUITS"], ["RAISIN", "FRUITS"], ["ORANGE", "FRUITS"],
-  ["RENAULT", "VOITURE"], ["PEUGEOT", "VOITURE"], ["FERRARI", "VOITURE"], ["LAMBORGHINI", "VOITURE"], ["CITROEN", "VOITURE"], ["PORSCHE", "VOITURE"],
-  ["CITROUILLE", "HALLOWEEN"], ["SORCIERE", "HALLOWEEN"], ["DRACULA", "HALLOWEEN"], ["ZOMBIE", "HALLOWEEN"], ["VAMPIRE", "HALLOWEEN"], ["FRANKENSTEIN", "HALLOWEEN"]
-];
+class HangmanGame
+{
 
-function init() {
-  erreur = 0;
-  stockageMot = [];
-  pendu.src = "images/pendu.png";
-  reset();
-  nouvellePartie();
-};
+  words = [
+    ["BASEBALL", "SPORT"], ["TENNIS", "SPORT"], ["PADDLE", "SPORT"], ["JUDO", "SPORT"], ["BASKETBALL", "SPORT"], ["KARATE", "SPORT"],
+    ["FRANCE", "PAYS"], ["ESPAGNE", "PAYS"], ["ALLEMAGNE", "PAYS"], ["CANADA", "PAYS"], ["ANGLETERRE", "PAYS"], ["UKRAINE", "PAYS"],
+    ["POMME", "FRUITS"], ["FRAISE", "FRUITS"], ["BANANE", "FRUITS"], ["ABRICOT", "FRUITS"], ["RAISIN", "FRUITS"], ["ORANGE", "FRUITS"],
+    ["RENAULT", "VOITURE"], ["PEUGEOT", "VOITURE"], ["FERRARI", "VOITURE"], ["LAMBORGHINI", "VOITURE"], ["CITROEN", "VOITURE"], ["PORSCHE", "VOITURE"],
+    ["CITROUILLE", "HALLOWEEN"], ["SORCIERE", "HALLOWEEN"], ["DRACULA", "HALLOWEEN"], ["ZOMBIE", "HALLOWEEN"], ["VAMPIRE", "HALLOWEEN"], ["FRANKENSTEIN", "HALLOWEEN"]
+  ];
 
-function genererNombreEntier(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
+  lastWordIndex = null;
+  gameStarted = false;
+  errorsCount = 0;
+  foundLetters = 0;
+  selectedWord = null
+  isAnswering = false;
 
-function nouvellePartie() {
+  wordContainer = document.querySelector('#mot');
+  keyboardContainer = document.querySelector('#lettre_grille');
+  hangmanImage = document.querySelector('#pendu');
+  newGameButton = document.querySelector('#partie');
+  answerForm = document.querySelector('#formulaire');
+  answerInput = document.querySelector('#devine');
+  indiceContainer = document.querySelector('#indice');
 
-  do {
-    nombreAleatoire = genererNombreEntier(mots.length);
-  } while (nombreAleatoire == dernier)
 
-  indice.textContent = mots[nombreAleatoire][1];
-  dernier = nombreAleatoire;
-  leMot = mots[nombreAleatoire][0];
-  demarrer.style.display = 'none';
-  masquerMot();
-};
+  constructor() {
 
-function masquerMot() {
-  for (let i = 0; i < leMot.length; i++) {
-    stockageMot[i] = leMot[i].replace(/[A-Z]/, "_");
-    mot.textContent = stockageMot.join("");
-  }
-};
+    this.renderKeyboad(this.keyboardContainer);
 
-function gagner() {
-  pendu.src = "images/win.png";
-  demarrer.style.display = 'block';
-};
+    this.answerInput.addEventListener('focus', () => {
+      this.isAnswering = true;
+    });
 
-function gameOver() {
-  mot.textContent = leMot;
-  pendu.src = "images/gameover.png";
-  demarrer.style.display = 'block';
-};
+    this.answerInput.addEventListener('blur', () => {
+      this.isAnswering = false;
+    });
 
-function echec() {
-  if (erreur == 1) {
-    pendu.src = "images/pendu_1.png";
-    input.value = "";
-  }
-  else if (erreur == 2) {
-    pendu.src = "images/pendu_2.png";
-    input.value = "";
-  }
-  else if (erreur == 3) {
-    pendu.src = "images/pendu_3.png";
-    input.value = "";
-  }
-  else if (erreur == 4) {
-    pendu.src = "images/pendu_4.png";
-    input.value = "";
-  }
-  else if (erreur == 5) {
-    pendu.src = "images/pendu_5.png";
-    input.value = "";
-  }
-  else if (erreur == 6) {
-    pendu.src = "images/pendu_6.png";
-    input.value = "";
-    window.setTimeout(function attendre() { gameOver(); }, 1000);
-  }
-};
-
-function submit() {
-  formulaire.addEventListener('submit', (e) => {
-    e.preventDefault();
-    if (input.value == '') {
-      input.style.borderColor = "red";
-    }
-    else if (input.value == leMot) {
-      input.style.borderColor = "silver";
-      input.value = "";
-      mot.textContent = leMot;
-      gagner()
-    }
-    else if(input.value != leMot) {
-      echec(erreur++);
-      input.style.borderColor = "silver";
-    }
-  });
-};
-
-[].forEach.call(document.querySelectorAll('.lettre'), function(div) {
-  div.addEventListener('click', function(e) {
-    if (!this.classList.contains('disabled')) {
-      this.classList.add('disabled');
-    }
-  });
-});
-
-function reset() {
-  [].forEach.call(document.querySelectorAll('.lettre'), function(o) {
-    o.classList.remove('disabled');
-  });
-}
-
-function click() {
-  for (let i = 0, lettres; lettres = alphabet[i]; i++) {
-    alphabet[i].addEventListener('click', () => {
-      lettres = alphabet[i].firstChild.nodeValue;
-      if (leMot.includes(lettres)) {
-        for (let i = 0; i < leMot.length; i++) {
-
-          if (leMot[i] == lettres) {
-            stockageMot[i] = leMot[i];
-            mot.textContent = stockageMot.join("");
-          }
-        }
-      } else {
-        echec(erreur++);
+    document.body.addEventListener('keydown', (event) => {
+      if(this.isAnswering) {
+        return;
       }
-      if (mot.textContent == leMot) {
-        gagner()
+
+      if (event.key.match(/^[a-z]$/i)) {
+        const letter = event.key.toUpperCase();
+        const button = document.querySelector(`#letter_${letter}`);
+        button.click();
+      }
+      if (event.key === 'Enter' && this.gameStarted === false) {
+        this.start();
       }
     });
+
+    this.newGameButton.addEventListener('click', () => {
+      this.start();
+    });
+
+    this.answerForm.addEventListener('submit', (event) => {
+      this.handleAnswerFormSubmit(event);
+    });
+
+    this.start();
   }
-};
 
-demarrer.addEventListener('click', init);
+  handleAnswerFormSubmit(event) {
+    event.preventDefault();
+    if (this.answerForm.devine.value === '') {
+      return;
+    }
 
-click();
-submit();
+    const answer = this.answerInput.value;
+    this.answerInput.value = '';
+
+    if (answer === this.selectedWord[0]) {
+      this.win();
+      return
+    }
+
+    this.errorsCount++;
+    this.renderHangman();
+  }
+
+  getRandomWord() {
+    let randomIndex;
+    do {
+      randomIndex = Math.floor(Math.random() * this.words.length);
+    } while (randomIndex === this.lastWordIndex);
+    this.lastWordIndex = randomIndex;
+
+    return this.words[randomIndex];
+  }
+
+  handleLetterClick(event) {
+    if (!this.gameStarted) {
+      return;
+    }
+
+    const button = event.currentTarget;
+    if (button.classList.contains('disabled')) {
+      return;
+    }
+
+    button.classList.add('disabled');
+    const letter = button.textContent;
+    this.checkLetter(letter)
+  }
+
+  checkLetter(letter) {
+    let isLetterFound = false;
+    for (let i = 0; i < this.selectedWord[0].length; i++) {
+      if (this.selectedWord[0][i] === letter) {
+        this.wordContainer.children[i].textContent = letter;
+        this.foundLetters++;
+        isLetterFound = true;
+      }
+    }
+
+    if (!isLetterFound) {
+      this.errorsCount++;
+      this.renderHangman();
+
+      return false;
+    }
+
+    if (this.foundLetters === this.selectedWord[0].length) {
+      this.win();
+    }
+    return true;
+  }
+
+  renderKeyboad(container) {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    alphabet.forEach(letter => {
+      const button = document.createElement('button');
+      button.classList.add('lettre');
+      button.textContent = letter;
+      button.id = `letter_${letter}`
+      button.addEventListener('click', (event) => {
+        this.handleLetterClick(event)
+      });
+      container.appendChild(button);
+    });
+  }
+
+  renderSolution() {
+    for (let i = 0; i < this.selectedWord[0].length; i++) {
+      this.wordContainer.children[i].textContent = this.selectedWord[0][i];
+    }
+  }
+
+  renderHangman() {
+    this.hangmanImage.src = `images/pendu_${this.errorsCount}.png`;
+    if (this.errorsCount === 6) {
+      this.loose();
+    }
+  }
+
+  start() {
+    this.gameStarted = true;
+    this.wordContainer.innerHTML = '';
+    this.errorsCount = 0;
+    this.foundLetters = 0;
+    this.answerForm.style.display = 'block';
+    this.newGameButton.style.display = 'none';
+    this.hangmanImage.src = "images/pendu.png";
+
+    this.selectedWord = this.getRandomWord();
+    this.indiceContainer.textContent = this.selectedWord[1];
+
+    document.querySelectorAll('.lettre').forEach(element => {
+      element.classList.remove('disabled');
+    });
+
+    for (let i = 0; i < this.selectedWord[0].length; i++) {
+      const placeholder = document.createElement('span');
+      placeholder.classList.add('placeholder');
+      placeholder.textContent = '_';
+      this.wordContainer.appendChild(placeholder);
+    }
+  }
+
+  win() {
+    this.gameEnd();
+    this.hangmanImage.src = "images/win.png";
+
+  }
+
+  loose() {
+    this.gameEnd();
+    setTimeout(() => {
+      this.hangmanImage.src = "images/gameover.png";
+    }, 1000);
+  }
+
+  gameEnd() {
+    this.renderSolution();
+    this.gameStarted = false;
+    this.answerForm.style.display = 'none';
+    this.newGameButton.style.display = 'block';
+  }
+}
+
+const game = new HangmanGame();
+
